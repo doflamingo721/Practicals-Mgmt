0cda2512815c38e247fcdef1832adc9b56301f10
Jay Dixit <34883177+shanks6499@users.noreply.github.com>
2018-02-21 15:28:55 +0530

    Added Basic GUI
    Added basic GUI for adding functionalities.

---
+++ Admin/addClassMaster.php
@@ -0,0 +1,4 @@
+<html>
+	Enter Class Name:<input type="text" id="clname" name="clname"><br>
+	<input type="submit" value="submit"> 
+</html>
---
+++ Admin/addStudentMaster.php
@@ -0,0 +1,6 @@
+<html>
+	Enter Enrollment Number:<input type="text" id="enrollment" name="enrollment"><br>
+	Enter First Name:<input type="text" id="fname" name="fname"><br>
+	Enter Last Name:<input type="text" id="lname" name="lname"><br>
+	<input type="submit" value="submit"> 
+</html>
