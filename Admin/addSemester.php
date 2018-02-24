78f7ce4888e0074a6cabc6ecf4a9a928dcfc8482
AD1310 <atharvadeshpande99@gmail.com>
2018-02-24 14:45:02 +0530

    Register current Semester to DB

--- Admin/addSemester.php
+++ Admin/addSemester.php
@@ -1,1 +1,13 @@
-// Add Semester 
+<HTML>
+Add Current Semester<br><br>
+	<form>
+		Select Semester Number: <select id="semesterNumbers">
+						<option id="semesterNo" name="courseNo"></option>
+					  </select> <br><br>
+		Select Semester Name: <select id="semesters">
+						  <option id="semesterName" name="semesterName"></option>
+					     </select> <br><br>
+		<input type="submit" value="Confirm Semester">
+					
+	</form>
+</HTML>
