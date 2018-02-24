3114b4d2638bb16d77fdc569d2f8b86d2eaf0647
AD1310 <atharvadeshpande99@gmail.com>
2018-02-24 14:49:48 +0530

    Added division (class) to specify semester

--- Admin/addSemester.php
+++ Admin/addSemester.php
@@ -1,12 +1,15 @@
 <HTML>
 Add Current Semester<br><br>
 	<form>
+		Select Class: <select id="classes">
+						<option id="className" name="className"></option>
+					  </select><br><br>
 		Select Semester Number: <select id="semesterNumbers">
 						<option id="semesterNo" name="courseNo"></option>
 					  </select> <br><br>
 		Select Semester Name: <select id="semesters">
 						  <option id="semesterName" name="semesterName"></option>
-					     </select> <br><br>
+					     </select> <br><br>		
 		<input type="submit" value="Confirm Semester">
 					
 	</form>
