aa663b26d6768c59f6132f8531afc2993bbf34fd
Unknown <shrutivd135@gmail.com>
2018-02-24 15:05:31 +0530

    added basic faculty skeleton files
    addAssignmentmaster addNotice and showSubmissions files updated

--- Faculty/addAssignmentMaster.php
+++ Faculty/addAssignmentMaster.php
@@ -1,1 +1,39 @@
-// Add Assignment
+<html>
+<body>
+Add assignment<br><br>
+
+<form action="" method="">
+
+		Select Course 
+			<select id="courses">
+				<option id="courseName"></option>
+			</select>
+			
+		Select Division
+			<select id="divisions">
+				<option id="divisionName"></option>
+			</select>
+			
+		Select Batch
+			<select id="classes">
+				<option id="batchName"></option>
+			</select>
+			
+		Assignment Question
+			<input type="text" id="assignmentQuestion">
+			
+		Description
+			<input type="text" id="assignmentDescription">
+			
+		Start Date
+			<input type="date" id="startDate">
+			
+		End Date
+			<input type="date" id="endDate">
+			
+		<input type="submit" >
+		
+</form>
+	
+<body>
+</html>
